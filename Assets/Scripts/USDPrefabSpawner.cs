e928f639de168bff4391361bbc6e28dc8b3fa0d2
Haruka Fujisawa <Haruka.Fujisawa@sony.com>
2025-10-28 14:28:16 +0900

    Rename Script.

--- Assets/Scripts/USDPrefabSpawner.cs
+++ Assets/Scripts/USDPrefabSpawner.cs
@@ -1,9 +1,9 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class KitchenSetSpawner : NetworkBehaviour
+public class USDPrefabSpawner : NetworkBehaviour
 {
-  [SerializeField] private NetworkObject kitchenPrefab; // Kitchen_set_net を割り当て
+  [SerializeField] private NetworkObject usdPrefab; // USDプレハブを割り当て
   [SerializeField] private Vector3 spawnPosition = new Vector3(0f, 0f, 0f);
   [SerializeField] private Vector3 spawnRotation = new Vector3(90f, 0f, 0f);
   [SerializeField] private Vector3 spawnScale = new Vector3(1.0f, 1.0f, 1.0f);
@@ -11,7 +11,7 @@ public class KitchenSetSpawner : NetworkBehaviour
   public override void OnNetworkSpawn()
   {
     if (!IsServer) return;
-    var obj = Instantiate(kitchenPrefab, spawnPosition, Quaternion.Euler(spawnRotation));
+    var obj = Instantiate(usdPrefab, spawnPosition, Quaternion.Euler(spawnRotation));
     obj.transform.localScale = spawnScale;
     obj.Spawn(true); // 全クライアントへ出現
   }
